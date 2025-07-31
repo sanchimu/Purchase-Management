dfa98bdb2e6223c4b887b1a509b46e526de15c07
choihana <h112s@naver.com>
2025-07-31 17:41:12 +0900

    주석 단 버전(이렇게 하는 거 아니면 알려주세요..)

--- PurchasingManagement/src/main/java/jdbc/JdbcUtil.java
+++ PurchasingManagement/src/main/java/jdbc/JdbcUtil.java
@@ -7,27 +7,25 @@ import java.sql.Statement;
 
 public class JdbcUtil {
 	
-	public static void close(ResultSet rs) {
-		if(rs != null) {
+	public static void close(ResultSet rs) { // ResultSet을 안전하게 닫는 메서드(ResultSetを安全に閉じるメソッド)
+		if(rs != null) { // ResultSet이 null이 아닐 경우에만 (rsがnullでない場合のみ処理)
 			try {
 				rs.close();
-			} catch (SQLException ex) {
-				// TODO: handle exception
+			} catch (SQLException ex) { // 예외가 발생해도 무시 (例外が発生しても何もしない)
 			}
 		}
 	}
 	
-	public static void close(Statement stmt) {
+	public static void close(Statement stmt) { // Statement 또는 PreparedStatement를 안전하게 닫는 메서드(Statement（またはPreparedStatement）を安全に閉じるメソッド)
 		if(stmt != null) {
 			try {
 				stmt.close();
 			} catch (SQLException ex) {
-				// TODO: handle exception
 			}
 		}
 	}
 	
-	public static void close(Connection conn) {
+	public static void close(Connection conn) { // Connection 을 안전하게 닫는 메서드 (Connectionを安全に閉じるメソッド)
 		if(conn!=null) {
 			try {
 				conn.close();
@@ -36,12 +34,11 @@ public class JdbcUtil {
 		}
 	}
 	
-	public static void rollback(Connection conn) {
+	public static void rollback(Connection conn) { // 롤백 처리 메서드 (ロールバックするメソッド)
 		if(conn!=null) {
 			try {
-				conn.rollback();
+				conn.rollback(); // 롤백 실행=트랜잭션 취소 (トランザクションを取り消す)
 			} catch (SQLException ex) {
-				// TODO: handle exception
 			}
 		}
 	}
